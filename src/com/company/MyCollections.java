0f2e7623b609c2803bda4ab18749b7dec9d4615a
arunkudiyal <arunkudiyal@icloud.com>
2023-01-21 11:07:56 +0530

    Introduction to Wrapper Classes

---
+++ src/com/company/MyCollections.java
@@ -0,0 +1,11 @@
+package com.company;
+
+// Java Collections --> Collection of classes & interfaces which helps you to perform data-manipulation very easily.
+
+public class MyCollections {
+    public static void main(String[] args) {
+        // Wrapper Class
+        Integer myInt = new Integer(100);
+        System.out.println(myInt);
+    }
+}
